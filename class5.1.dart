36cbbf56260f7ccb10040b12734422cb9be7f0c7
ali muhammad <9291ali.muhammad@gmail.com>
2023-09-16 03:28:12 -0700

    table

--- class5.1.dart
+++ class5.1.dart
@@ -1,7 +1,18 @@
 void main (){
+int no = 3;
+for(int i=1;i<=10;i++){
 
-print("Ali Muhammad Aziz");
 
+  print("table");
+
+for(int j=1;j<=10;j++){
+
+print("$i x $j = ${10*j}");
+
+}
 
 
 }
+
+
+} 
